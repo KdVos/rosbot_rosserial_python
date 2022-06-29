76a52282ed9233f0dc132875f6ba361919b4aa8e
Koen de Vos <k.d.vos@tue.nl>
2022-06-29 00:56:37 -0700

    working prototype

--- nodes/serial_node.py
+++ nodes/serial_node.py
@@ -36,7 +36,7 @@
 __author__ = "mferguson@willowgarage.com (Michael Ferguson)"
 
 import rospy
-from rosserial_python import SerialClient, RosSerialServer
+from rosbot_rosserial_python import SerialClient, RosSerialServer
 from serial import SerialException
 from time import sleep
 import multiprocessing
@@ -49,6 +49,7 @@ if __name__=="__main__":
 
     port_name = rospy.get_param('~port','/dev/ttyUSB0')
     baud = int(rospy.get_param('~baud','57600'))
+    tf_prefix = rospy.get_param('~tf_prefix','')
 
     # for systems where pyserial yields errors in the fcntl.ioctl(self.fd, TIOCMBIS, \
     # TIOCM_DTR_str) line, which causes an IOError, when using simulated port
@@ -92,7 +93,7 @@ if __name__=="__main__":
         while not rospy.is_shutdown():
             rospy.loginfo("Connecting to %s at %d baud" % (port_name,baud) )
             try:
-                client = SerialClient(port_name, baud, fix_pyserial_for_test=fix_pyserial_for_test)
+                client = SerialClient(port_name, baud, fix_pyserial_for_test=fix_pyserial_for_test, tf_prefix=tf_prefix)
                 client.run()
             except KeyboardInterrupt:
                 break
